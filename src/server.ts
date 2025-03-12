a4dbde645fd8698ba9624bdc02e24c72b8dc4bba
GeekyGeeky <therealolushola@gmail.com>
2025-03-12 05:17:17 +0100

    chore: updated webhook config

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,18 @@ export const startServer = () => {
   app.use(express.json());
 
   // Set up the webhook endpoint
-  app.post(`/${process.env.BOT_TOKEN}`, webhookCallback(bot, "express"));
+  // app.use(webhookCallback(bot, "express"));
 
+  // Set webhook URL (replace with your actual HTTPS URL)
+  // app.post(`/${process.env.BOT_TOKEN}`, webhookCallback(bot, "express"));
+
+  app.use("/bot", webhookCallback(bot, "express")); // Attach webhook to /bot
+
+  
   // Start the server
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
+    bot.api.setWebhook(process.env.APP_URL || "https://b7d6-102-89-82-107.ngrok-free.app/bot");
     console.log(`Webhook server is running on port ${PORT}`);
   });
 };
