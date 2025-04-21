ca0ce8c7effcd5b9054c0d3a8fe56a426b763af4
GeekyGeeky <therealolushola@gmail.com>
2025-04-21 01:17:44 +0100

    chore: updated config; central store for all config

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@ dotenv.config();
 
 export const BOT_TOKEN = process.env.BOT_TOKEN || '';
 export const PORT = process.env.PORT || '3000';
+export const WEBHOOK_URL = process.env.WEBHOOK_URL;
+export const NODE_ENV = process.env.NODE_ENV;
