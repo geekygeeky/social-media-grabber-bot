aef927dbc76c31312c7df0749edb5aa87e75650b
GeekyGeeky <therealolushola@gmail.com>
2025-07-10 02:01:58 +0100

    chore: add missing environment variables for IG and TikTok downloader URLs

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,6 @@ export const BOT_TOKEN = process.env.BOT_TOKEN || '';
 export const PORT = process.env.PORT || '3000';
 export const APP_URL = process.env.APP_URL;
 export const NODE_ENV = process.env.NODE_ENV;
+export const IG_DOWNLOADER_URL = process.env.IG_DOWNLOADER_URL;
+export const TIKTOK_DOWNLOADER_URL = process.env.TIKTOK_DOWNLOADER_URL;
+
