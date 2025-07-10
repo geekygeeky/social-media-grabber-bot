7a2455815aa620faf969f0c180c963b48e778768
GeekyGeeky <therealolushola@gmail.com>
2025-07-10 19:49:23 +0100

    feat: added headers to fetch request for youtube video download * removed ytdl core

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,4 @@
 import { Bot, Context, InputFile } from "grammy";
-import ytdl from "@distube/ytdl-core";
 import {
   BOT_TOKEN,
   PORT as port,
@@ -48,14 +47,24 @@ bot.on("message", async (ctx: Context) => {
     } else if (url.includes("youtube.com") && url.includes("shorts")) {
       // Handle Youtube Shorts URL
       const result = await downloader.youtube(url);
-      const videoStream = ytdl(url);
-      // const response = await fetch(result.mp4);
+      // const videoStream = ytdl(url);
+      const headers = {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        Referer: "https://www.youtube.com/",
+      };
+      const response = await fetch(result.mp4, { headers });
 
-     /*  if (!response.ok || !response.body) {
+      if (!response.ok || !response.body) {
         throw new Error(`Failed to fetch video: ${response.statusText}`);
-      } */
+      }
       const caption = `Author: ${result.author}\nTitle: ${result.title}`;
-      await ctx.replyWithVideo(new InputFile(videoStream), {
+      await ctx.replyWithVideo(new InputFile(response.body), {
         caption,
         cover: result.thumbnail,
       }); // Send video
